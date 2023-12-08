9e89f90601282cae9a27b36e9085a35a272afb82
Alana <alana_cho@brown.edu>
2023-12-08 09:37:21 -0500

    more fsm things

--- game.ino
+++ game.ino
@@ -1,6 +1,36 @@
 #include "game.h"
 
-int boardMap[SCI_LI_HEIGHT][SSCI_LI_WIDTH];
+int boardMap[SCI_LI_HEIGHT][SCI_LI_WIDTH];
+int snakeCoords[][];
+static byte x, y;
+static orientation o;
+static int timeStep, savedClock;
+static int mils;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  initializeSystem();
+
+  /*
+   * Initialize all FSM variables
+   */
+  timeStep = 1000;
+  mils = millis();
+  savedClock = mils;
+
+  // display something on the LCD
+  displayLevel(level, countdown);
+}
+
+void loop() {
+  static state CURRENT_STATE = sDISP_COUNTDOWN;
+  updateInputs();
+  CURRENT_STATE = updateFSM(CURRENT_STATE, millis(), numButtonsPressed, lastButtonPressed);
+  delay(10);
+}
+
 
 void testColorPoint(int row, int col, CRGB color) {
   xy ledLocation = calculateXY(row, col);
@@ -87,6 +117,22 @@ bool isIntoSelf(byte o, int lastButton) {
   }
 }
 
+// Need to check if moving into a food cell
+bool isEating(byte o, int lastButton) {
+  if (o == RIGHT and snakeCoords)
+}
+
+
+// Called each time the snake moves
+void move() {
+
+}
+
+// Called when the snake is moving and eating
+void moveAndEat() {
+
+}
+
 // Game over function turns the entire board red
 void gameOver() {
   for (int i = 0; i < SCI_LI_HEIGHT){
@@ -102,14 +148,40 @@ state updateFSM(state currState, long mils, int numButtons, int lastButton) {
   mils = millis();
   switch(curState) {
     case WAIT_START:
-    break;
+      if (mils - savedClock >= timeStep) { // transition 1-2
+        initializeMap();
+        savedClock = mils;
+        nextState = MOV;
+      }
+      break;
     case MOV:
-    break;
+      if ((mils - savedClock >= timeStep) and !(facingWall() or isEating()) { // NOTE FROM ALANA: idk what to pass into facingWall, transition 2-2
+        move();
+        savedClock = mils;
+        nextState = MOV;
+      }
+      else if ((mils - savedClock >= timeStep) and isEating()) { // transition 2-3
+        moveAndEat();
+        savedClock = mils;
+        nextState = EATING;
+      }
+      else if ((mils - savedClock >= timeStep) and facingWall()) { // transition 2-4
+        gameOver();
+        nextState = GAME_OVER;
+      }
+      break;
     case EATING:
-    break;
+      if (mils - savedClock >= 500) { // transition 3-2
+        updateGame();
+        nextState = MOV;
+      }
+      break;
     case GAME_OVER:
-    break;
-    case DEFAULT:
-    break;
+      if () { // trigger on reset button interrupt while in game_over state, transition 4-1
+        initializeMap();
+      }
+      break;
+    case DEFAULT: // NOTE FROM ALANA: what do we want to do here??
+      break;
   }
 } 
