d02fc7ac14ba24c80457f072bc72d27c99172612
Alana <alana_cho@brown.edu>
2023-12-09 15:43:25 -0500

    more game logic

--- game.ino
+++ game.ino
@@ -2,10 +2,11 @@
 
 int boardMap[SCI_LI_HEIGHT][SCI_LI_WIDTH];
 int snakeCoords[][];
-static byte x, y;
 static orientation o;
 static int timeStep, savedClock;
 static int mils;
+static int score;
+static int length;
 
 void setup() {
   Serial.begin(9600);
@@ -41,8 +42,8 @@ void testColorPoint(int row, int col, CRGB color) {
 void initializeMap(){
 
   // Sets all cells on the board to be plain cells
-  for (int i = 0; i < SCI_LI_HEIGHT){
-    for (int j = -; j < SCI_LI_WIDTH){
+  for (int i = 0; i < SCI_LI_HEIGHT, i++){
+    for (int j = -; j < SCI_LI_WIDTH, j++){
       boardMap[i][j] = FLAG_PLAIN_CELL;
     }
   }
@@ -52,12 +53,8 @@ void initializeMap(){
 
   // Places the snake at a random point on the board
   boardMap[snake_row][snake_col] = FLAG_SNAKE;
+  length = 1;
   testColorPoint(snake_row, snake_col, CRGB::White);
-
-  // int exampleSnakeCoords[1][2] = {
-  //   { snake_row, snake_col}
-  // };
-  // exampleColorSnake(exampleSnakeCoord, 1)
 
   placeFood();
 }
@@ -68,10 +65,9 @@ void placeFood(){
   int food_col = rand() % SCI_LI_WIDTH;
 
   // Checks that the snake is not in the randomly-chosen cell
-  if boardMap[food_row][food_col] == FLAG_PLAIN_CELL{
+  if (boardMap[food_row][food_col] == FLAG_PLAIN_CELL) {
     boardMap[food_row][food_col] = FLAG_FOOD;
-    testColorPoint(snake_row, snake_col, CRGB::Green);
-
+    testColorPoint(food_row, food_col, CRGB::Green);
   } else {
     placeFood(); // Recurse until we find a plain cell
   }
@@ -119,18 +115,102 @@ bool isIntoSelf(byte o, int lastButton) {
 
 // Need to check if moving into a food cell
 bool isEating(byte o, int lastButton) {
-  if (o == RIGHT and snakeCoords)
+  int nextHeadRow = snakeCoords[0][0];
+  int nextHeadCol = snakeCoords[0][1];
+
+  // Calculate the next head position based on the current orientation
+  switch (o) {
+    case UP:
+      nextHeadRow--;
+      break;
+    case DOWN:
+      nextHeadRow++;
+      break;
+    case RIGHT:
+      nextHeadCol++;
+      break;
+    case LEFT:
+      nextHeadCol--;
+      break;
+  }
+
+  // Check if the next head position contains food
+  if (boardMap[nextHeadRow][nextHeadCol] == FLAG_FOOD) {
+
+    return true;
+  }
+
+  return false;
 }
 
 
 // Called each time the snake moves
-void move() {
+void move(byte o) {
+  // Save the current head position
+  int currentHeadRow = snakeCoords[0][0];
+  int currentHeadCol = snakeCoords[0][1];
 
+  // Update the head position based on the current orientation
+  switch (o) {
+    case UP:
+      currentHeadRow--;
+      break;
+    case DOWN:
+      currentHeadRow++;
+      break;
+    case RIGHT:
+      currentHeadCol++;
+      break;
+    case LEFT:
+      currentHeadCol--;
+      break;
+  }
+
+  boardMap[currentHeadRow][currentHeadCol] = FLAG_SNAKE;
+  testColorPoint(currentHeadRow, currentHeadCol, CRGB::White);
+  snakeCoords[0][0] = currentHeadRow;
+  snakeCoords[0][1] = currentHeadCol;
+
+  // Remove the tail from the boardMap and snakeCoords
+  int tailRow = snakeCoords[length - 1][0];
+  int tailCol = snakeCoords[length - 1][1];
+  boardMap[tailRow][tailCol] = FLAG_PLAIN_CELL;
+  // Update the rest of the snakeCoords array by shifting elements
+  for (int i = length - 1; i > 0; i--) {
+    snakeCoords[i][0] = snakeCoords[i - 1][0];
+    snakeCoords[i][1] = snakeCoords[i - 1][1];
+  }
 }
 
 // Called when the snake is moving and eating
-void moveAndEat() {
+void moveAndEat(byte o) {
+  // Save the current head position
+  int currentHeadRow = snakeCoords[0][0];
+  int currentHeadCol = snakeCoords[0][1];
 
+  // Update the head position based on the current orientation
+  switch (o) {
+    case UP:
+      currentHeadRow--;
+      break;
+    case DOWN:
+      currentHeadRow++;
+      break;
+    case RIGHT:
+      currentHeadCol++;
+      break;
+    case LEFT:
+      currentHeadCol--;
+      break;
+
+  boardMap[currentHeadRow][currentHeadCol] = FLAG_SNAKE;
+  testColorPoint(currentHeadRow, currentHeadCol, CRGB::White);
+  snakeCoords[0][0] = currentHeadRow;
+  snakeCoords[0][1] = currentHeadCol;
+
+  score++;
+  length++;
+  placeFood();
 }
 
 // Game over function turns the entire board red
@@ -155,24 +235,24 @@ state updateFSM(state currState, long mils, int numButtons, int lastButton) {
       }
       break;
     case MOV:
-      if ((mils - savedClock >= timeStep) and !(facingWall() or isEating()) { // NOTE FROM ALANA: idk what to pass into facingWall, transition 2-2
-        move();
+      if ((mils - savedClock >= timeStep) and !(facingWall() or isEating() or isIntoSelf())) { // NOTE FROM ALANA: idk what to pass into facingWall, transition 2-2
+        move(o);
         savedClock = mils;
         nextState = MOV;
       }
       else if ((mils - savedClock >= timeStep) and isEating()) { // transition 2-3
-        moveAndEat();
+        moveAndEat(o);
         savedClock = mils;
         nextState = EATING;
       }
-      else if ((mils - savedClock >= timeStep) and facingWall()) { // transition 2-4
+      else if ((mils - savedClock >= timeStep) and (facingWall() or isIntoSelf()) { // transition 2-4
         gameOver();
         nextState = GAME_OVER;
       }
       break;
     case EATING:
       if (mils - savedClock >= 500) { // transition 3-2
-        updateGame();
+        move(o);
         nextState = MOV;
       }
       break;
